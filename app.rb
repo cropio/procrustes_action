612a832b097528144e111e1db69ff1fb31801c83
ozeron <ozeron@me.com>
2019-03-12 10:33:33 +0200

    Make ENV fetch

--- app.rb
+++ app.rb
@@ -8,11 +8,13 @@ require 'byebug'
 require_relative './lib/loc_limiter'
 Dotenv.load
 
-# GITHUB TOKEN PATH
-ACCESS_TOKEN = ENV['GITHUB_TOKEN']
-GITHUB_EVENT_PATH = ENV.fetch('GITHUB_EVENT_PATH')
+# ENV CONFIG
 
+ACCESS_TOKEN = ENV.fetch('GITHUB_TOKEN')
+GITHUB_EVENT_PATH = ENV.fetch('GITHUB_EVENT_PATH')
 ADDITIONS_MAX_NUM = ENV.fetch('ADDITIONS_MAX_NUM', 1000).to_i
+
+puts "RUNNIG PR CHECK WITH MAX SIZE #{ADDITIONS_MAX_NUM}"
 ###### APP PART #####
 
 @client ||= Octokit::Client.new(access_token: ACCESS_TOKEN)
